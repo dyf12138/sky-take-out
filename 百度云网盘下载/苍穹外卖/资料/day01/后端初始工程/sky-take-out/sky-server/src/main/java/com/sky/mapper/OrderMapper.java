a4ee35c09b0b1c899f6124af0bc2817c13ac3a6d
dyf12138 <1537657319@qq.com>
2024-03-13 14:32:15 +0800

    Spring Task定时任务代码开发

--- 百度云网盘下载/苍穹外卖/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ 百度云网盘下载/苍穹外卖/资料/day01/后端初始工程/sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -7,6 +7,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     /**
@@ -40,4 +43,12 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id=#{id}")
     Orders getById(Long id);
+
+    /**
+     * 根据状态和下单时间查询订单
+     * @param status
+     * @param orderTime
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
 }
